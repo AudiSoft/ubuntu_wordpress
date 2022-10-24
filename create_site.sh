c28ce81c5d686cfeee7536f36504ca0ee41d5ba6
Jorge Arbelaez <jearbelaezr@audisoft.com>
2022-10-24 08:38:26 -0500

    Execute as sudo

--- create_site.sh
+++ create_site.sh
@@ -5,16 +5,16 @@ sudo chown -R $USER:$USER /var/www/$1
 # Create index.html
 echo "<h1>Welcome to $1</h1>"> /var/www/$1/html/index.html
 # Create configuration file for site
-echo "server {" > /etc/nginx/sites-available/$1
-echo "        listen 80;" > /etc/nginx/sites-available/$1
-echo "        listen [::]:80;" > /etc/nginx/sites-available/$1
-echo "" > /etc/nginx/sites-available/$1
-echo "        root /var/www/$1/html;" > /etc/nginx/sites-available/$1
-echo "        index index.html index.htm index.nginx-debian.html;" > /etc/nginx/sites-available/$1
-echo "" > /etc/nginx/sites-available/$1
-echo "        server_name $1 www.$1;" > /etc/nginx/sites-available/$1
-echo "" > /etc/nginx/sites-available/$1
-echo "        location / {" > /etc/nginx/sites-available/$1
-echo "                try_files $uri $uri/ =404;" > /etc/nginx/sites-available/$1
-echo "        }"
-"}"
+sudo echo "server {"                                                    >> /etc/nginx/sites-available/$1
+sudo echo "        listen 80;"                                          >> /etc/nginx/sites-available/$1
+sudo echo "        listen [::]:80;"                                     >> /etc/nginx/sites-available/$1
+sudo echo ""                                                            >> /etc/nginx/sites-available/$1
+sudo echo "        root /var/www/$1/html;"                              >> /etc/nginx/sites-available/$1
+sudo echo "        index index.html index.htm index.nginx-debian.html;" >> /etc/nginx/sites-available/$1
+sudo echo ""                                                            >> /etc/nginx/sites-available/$1
+sudo echo "        server_name $1 www.$1;"                              >> /etc/nginx/sites-available/$1
+sudo echo ""                                                            >> /etc/nginx/sites-available/$1
+sudo echo "        location / {"                                        >> /etc/nginx/sites-available/$1
+sudo echo "                try_files $uri $uri/ =404;"                  >> /etc/nginx/sites-available/$1
+sudo echo "        }"                                                   >> /etc/nginx/sites-available/$1
+sudo echo "}"                                                           >> /etc/nginx/sites-available/$1
