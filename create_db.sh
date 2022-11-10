7cb4d5cf0efc3767c3e68bebd323590edfb87051
SoporteAudi <soporte@audisoft.com>
2022-11-10 16:57:52 -0500

    changes of names resources

--- create_db.sh
+++ create_db.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
-#login to mysql whit root
-#sudo mysql
-##create user
-#CREATE USER 'henutsen'@'localhost' IDENTIFIED BY '123';
-##asigne privileges to user
-#GRANT ALL PRIVILEGES ON * . * TO 'henutsen'@'localhost';
-#
-#exit 
-#
-#mysql -u henutsen -p123
-## create data base 
-#CREATE DATABASE test;
-
-sudo mysql < test.sql
+#login to mysql whit root for create db 
+sudo mysql < create_wordpress_db.sql
 
 sleep 20s
